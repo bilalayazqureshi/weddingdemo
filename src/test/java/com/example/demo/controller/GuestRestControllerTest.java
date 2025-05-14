98c8ba56aca3faca440106c6639a2c707c058e1f
Bilal <bilalayazqureshi>
2025-05-14 17:16:59 +0200

    failing test for REST GET

---
+++ src/test/java/com/example/demo/controller/GuestRestControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test; // Use JUnit 5's Test annotation
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = GuestRestController.class)
+class GuestRestControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void testAllGuestsEmpty() throws Exception {
+		this.mvc.perform(get("/api/guests").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().json("[]")); // Checks that the /api/guests endpoint returns an empty JSON list
+	}
+}
---
+++ src/test/java/com/example/demo/controller/WeddingEventRestControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = WeddingEventRestController.class)
+class WeddingEventRestControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void testAllWeddingEventEmpty() throws Exception {
+		this.mvc.perform(get("/api/weddingevent").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().json("[]")); // Checks that the /api/guests endpoint returns an empty JSON list
+	}
+}
